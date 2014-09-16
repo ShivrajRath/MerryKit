e91f769ab26ddbc8b834b0b23193bed21b592302
Shivraj Rath <shivrajrath@gmail.com>
2014-09-16 09:01:54 -0700

    Adding angular structure

--- app/routes/app/seller.app.routes.js
+++ app/routes/app/seller.app.routes.js
@@ -11,85 +11,85 @@ var mkcomon = require('../../utilities/mkcommon.js');
 
 module.exports = function(router, passport) {
 
-    router.use(function(req, res, next) {
-        console.log('In Seller router');
-        console.log('%s %s %s', req.method, req.url, req.path);
+  router.use(function(req, res, next) {
+    console.log('In Seller router');
+    console.log('%s %s %s', req.method, req.url, req.path);
 
-        next();
+    next();
+  });
+
+  // Home page for the seller
+  router.get('/', function(req, res) {
+    res.render('common/views/seller/home', {
+      title: 'Seller'
     });
+  });
 
-    // Home page for the seller
-    router.get('/', function(req, res) {
-        res.render('common/views/seller/home', {
-            title: 'Seller'
-        });
+  router.route('/login')
+    .get(function(req, res) {
+      res.render('common/views/seller/login', {
+        title: 'Login'
+      })
+    })
+    .post(passport.authenticate('local-login', {
+      successRedirect: '/seller/profile', // redirect to the secure profile section
+      failureRedirect: '/seller/signup', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    }))
+
+
+  router.route('/signup')
+    .get(function(req, res) {
+      res.render('common/views/seller/signup', {
+        title: 'Seller Signup'
+      });
+    })
+    .post(passport.authenticate('local-signup', {
+      successRedirect: '/seller/profile', // redirect to the secure profile section
+      failureRedirect: '/seller/signup', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    }));
+
+
+  router.get('/profile', isLoggedIn, function(req, res) {
+
+    res.render('common/views/seller/profile', {
+      title: 'Seller Profile',
+      user: req.user
     });
+  });
 
-    router.route('/login')
-        .get(function(req, res) {
-            res.render('common/views/seller/login', {
-                title: 'Login'
-            })
-        })
-        .post(passport.authenticate('local-login', {
-            successRedirect: '/seller/profile', // redirect to the secure profile section
-            failureRedirect: '/seller/signup', // redirect back to the signup page if there is an error
-            failureFlash: true // allow flash messages
-        }))
-
-
-    router.route('/signup')
-        .get(function(req, res) {
-            res.render('common/views/seller/signup', {
-                title: 'Seller Signup'
-            });
-        })
-        .post(passport.authenticate('local-signup', {
-            successRedirect: '/seller/profile', // redirect to the secure profile section
-            failureRedirect: '/seller/signup', // redirect back to the signup page if there is an error
-            failureFlash: true // allow flash messages
-        }));
-
-
-    router.get('/profile', isLoggedIn, function(req, res) {
-
-        res.render('common/views/seller/profile', {
-            title: 'Seller Profile',
-            user: req.user
-        });
+  // Adding a product category
+  router.get('/product/add', isLoggedIn, function(req, res) {
+    res.render('common/views/seller/product/add/add', {
+      title: 'Add a product',
+      categories: mkcomon.getCategories()
     });
+  });
 
-    // Adding a product category
-    router.get('/product/add', isLoggedIn, function(req, res) {
-        res.render('common/views/seller/product/add/add', {
-            title: 'Add a product',
-            categories: mkcomon.getCategories()
-        });
+  // Adding a product category
+  router.get('/product/add/:category', isLoggedIn, function(req, res) {
+
+    res.render('common/views/seller/profile', {
+      title: req.params.category
     });
+  });
 
-    // Adding a product category
-    router.get('/product/add/:category', isLoggedIn, function(req, res) {
-
-        res.render('common/views/seller/profile', {
-            title: req.params.category
-        });
-    });
-
-    router.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
+  router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 }
 
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated()) {
-        return next();
-    }
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
-    // if they aren't redirect them to the home page
-    res.redirect('/seller');
+  // if they aren't redirect them to the home page
+  res.redirect('/seller');
 }
--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ Description: Build rules for the project
 ***********************************************************/
 
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    beautify = require('gulp-beautify');
+  jshint = require('gulp-jshint'),
+  beautify = require('gulp-beautify');
 
 // Common project paths
 var paths = {
-    'src': ['./server.js', './app/**/*.js', './public/**/*.js'],
-    'tests': ['./test/**/*.js']
+  'src': ['./server.js', './app/**/*.js', './public/**/*.js'],
+  'tests': ['./test/**/*.js']
 };
 
 /**
@@ -20,18 +20,18 @@ var paths = {
 
 // lint source with jshint
 gulp.task('lint', function() {
-    return gulp.src(paths.src)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  return gulp.src(paths.src)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 // beautify javascript
 gulp.task('beautify', function() {
-    gulp.src(paths.src)
-        .pipe(beautify({
-            indentSize: 2
-        }))
-        .pipe(gulp.dest('./public/'))
+  gulp.src(paths.src)
+    .pipe(beautify({
+      indentSize: 2
+    }))
+    .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('default', ['lint']);
